40dc436e6978efd912e4a374886b29c3df04b610
Divya Gupta <divya.g@reachmobile.com>
2025-06-24 17:45:54 +0530

    Did minor changes in model

--- src/main/java/com/fastturtle/simpleUserCrudApp/models/AppUser.java
+++ src/main/java/com/fastturtle/simpleUserCrudApp/models/AppUser.java
@@ -1,19 +1,20 @@
 package com.fastturtle.simpleUserCrudApp.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "app_user")
 public class AppUser {
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "email")
     private String email;
 
     public int getId() {
